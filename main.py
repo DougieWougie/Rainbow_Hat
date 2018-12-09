52e01901f748836b3c8ee87d82660e8566f7b727
Dougie Richardson <dougthegreenie@gmail.com>
2018-12-09 22:30:07 +0000

    Added threads for the event handler and the rainbow cycle.

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-import datetime
+import colorsys
+from datetime import datetime
 import thread
 import time
-
 import rainbowhat as rh
+
+running = True
+
+
+def set_running():
+    global running
+    if running:
+        running = False
 
 
 def days(date_one, date_two):
-    date_one = datetime.datetime.strptime(date_one, "%Y-%m-%d")
-    date_two = datetime.datetime.strptime(date_two, "%Y-%m-%d")
+    date_one = datetime.strptime(date_one, "%Y-%m-%d")
+    date_two = datetime.strptime(date_two, "%Y-%m-%d")
     return abs((date_one - date_two).days)
 
 
-text = "SLEEPS UNTIL XMAS  " + str(days(datetime.datetime.today().strftime("%Y-%m-%d"), '2018-12-25'))
+text = "SLEEPS UNTIL XMAS  " + str(days(datetime.today().strftime("%Y-%m-%d"), '2018-12-25'))
 
 
 def clear():
@@ -49,6 +57,7 @@ def event_handler():
     @rh.touch.C.press()
     def touch_c(channel):
         rh.lights.rgb(0, 0, 1)
+        set_running()
 
 
 def blink(speed):
@@ -59,11 +68,33 @@ def blink(speed):
         time.sleep(speed)
 
 
-while True:
+def cycle_colours():
+    rh.rainbow.set_brightness(0.1)
+    for each in range(101):
+        r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb((each / 100.0), 1.0, 1.0)]
+        rh.rainbow.set_all(r, g, b)
+        rh.rainbow.show()
+
+
+def temperature():
+    return rh.weather.temperature()
+
+
+def pressure():
+    return rh.weather.pressure()
+
+
+try:
+    while running:
+        clear()
+
+        thread.start_new_thread(scroll, (text,))
+        thread.start_new_thread(event_handler, ())
+
+        while True:
+            # blink(0.1)
+            cycle_colours()
+except Exception:
+    pass
+finally:
     clear()
-
-    thread.start_new_thread(scroll, (text,))
-    thread.start_new_thread(event_handler, ())
-
-    while True:
-        blink(0.1)
